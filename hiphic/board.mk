2d9a1573b9d2630f876373a924e5b99dc68bfd04
Henry Miller Maia Bilby <bilby@motorola.com>
2023-02-28 17:02:28 -0400

    hiphic: bring up for T version using legacy variables
    Add BOARD_USES_LEGACY_CAMERA to use legacy camera per device
    
    Change-Id: I3ad03810cfb24e1fdbbc11d3b21ba58d9230f2dd
    mot-cam-category: BRING_UP
    Reviewed-on: https://gerrit.mot.com/2526379
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wellington Silva <limawell@motorola.com>
    Reviewed-by: Miguel Nicodemus <miguelh@motorola.com>
    Submit-Approved: Jira Key

--- hiphic/board.mk
+++ hiphic/board.mk
@@ -1,3 +1,5 @@
+ifeq ($(BOARD_USES_LEGACY_CAMERA), true)
+ifneq (,$(findstring hiphic, $(strip $(TARGET_PRODUCT))))
 # Build camera kernel driver
 ifneq ($(TARGET_USES_QMAA),true)
 ifneq ($(TARGET_BOARD_AUTO),true)
@@ -6,3 +8,5 @@ BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/camera.ko
 endif
 endif
 endif
+endif # TARGET_PRODUCT
+endif # BOARD_USES_LEGACY_CAMERA
