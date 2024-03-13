240291125b54441f7b43f926a7c32058396ebd3b
Max Marcio Fonseca Santos <maxmfs@motorola.com>
2024-03-13 08:17:56 -0300

    Changing Makefile vars to fix cn builds with legacy repos
    Camera legacy repos uses the TARGET_DEVICE as device folder name
    and not TARGET_PRODUCT. This mismatch causes cn builds to break.
    Using TARGET_DEVICE istead of TARGET_PRODUCT fixes this issue.
    
    Change-Id: If709814cf2e223e43524ad9d18789a10023cf483
    mot-cam-category: MISC
    Reviewed-on: https://gerrit.mot.com/2905298
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Joao Paulo Pereira de Vasconcellos Mendes <jpmendes@motorola.com>
    Reviewed-by: Chen Jiahao <jiahao@motorola.com>
    Reviewed-by: Miguel Nicodemus <miguelh@motorola.com>
    Submit-Approved: Jira Key

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ifeq ($(BOARD_USES_LEGACY_CAMERA), true)
 
-include $(call my-dir)/$(TARGET_PRODUCT)/Android.mk
+include $(call my-dir)/$(TARGET_DEVICE)/Android.mk
 
 endif # BOARD_USES_LEGACY_CAMERA
